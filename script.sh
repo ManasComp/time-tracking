4a223c765a712ccb5e940681d3387b298f9e4073
ManasComp <OndrejMan1@gmail.com>
2023-02-19 18:13:02 +0100

    docker name

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 docker build -t ondrejman/flaskr:latest . 
 
-docker container run -d -p 4995:4995 ondrejman/flaskr    
+docker container run -d  --name time_tracking -p 4995:4995 ondrejman/flaskr    
