38db5e44a8611b919533ff98cef1cc70ee694d85
ManasComp <OndrejMan1@gmail.com>
2023-03-29 21:54:43 +0200

    Formating

--- end-helper.py
+++ end-helper.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+import getopt
 import sqlite3
+import sys
+
 from flaskr import task
 
-import sys, getopt
 
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
+    """create a database connection to the SQLite database
         specified by the db_file
     :param db_file: database file
     :return: Connection object or None
@@ -19,24 +21,25 @@ def create_connection(db_file):
 
     return conn
 
+
 def main(argv):
-    category = ''
-    comment = ''
+    category = ""
+    comment = ""
     try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
-      print ('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
+        print("test.py -i <inputfile> -o <outputfile>")
+        sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+        if opt == "-h":
+            print("test.py -i <inputfile> -o <outputfile>")
             sys.exit()
         elif opt in ("-i", "--ifile"):
             category = arg
         elif opt in ("-o", "--ofile"):
             comment = arg
-    print ('Input file is "', category)
-    print ('Output file is "', comment)
+    print('Input file is "', category)
+    print('Output file is "', comment)
     database = r"instance/flaskr.sqlite"
 
     # create a database connection
@@ -49,4 +52,4 @@ def main(argv):
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
