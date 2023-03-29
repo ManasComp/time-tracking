676946b75748de1475bbe791f0ecba686e17e9ba
ManasComp <OndrejMan1@gmail.com>
2023-03-29 19:06:42 +0200

    Database backup

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,9 +1,12 @@
 """SQLite Database module for application."""
+import io
 import sqlite3
 
 import click
 from flask import current_app, g
 
+
+SERVER_ADDRESS = "/var/lib/docker/volumes/database/_data/backupdatabase.sql"
 
 def init_app(app):
     """Initialize the application with the database."""
@@ -22,8 +25,22 @@ def get_db():
     return g.db
 
 
+def backup_db():
+    """Backup the database."""
+    print("backing up db")
+    try:
+        with io.open(SERVER_ADDRESS, 'w') as p:
+            for line in get_db().iterdump():
+                p.write('%s\n' % line)
+    except:
+        print("backup failed")
+    print("backup done")
+
+
 def close_db(e=None):
     """Close the database connection."""
+    print("closing db")
+    backup_db()
     database = g.pop("db", None)
 
     if database is not None:
@@ -32,10 +49,20 @@ def close_db(e=None):
 
 def init_db():
     """Initialize the database."""
+
     database = get_db()
+
 
     with current_app.open_resource("schema.sql") as database_file:
         database.executescript(database_file.read().decode("utf8"))
+    
+    try:
+        with io.open(SERVER_ADDRESS, 'r') as p:
+            database.executescript(p.read())
+    except:
+        print("backup load failed")
+
+    backup_db()
 
 
 @click.command("init-db")
